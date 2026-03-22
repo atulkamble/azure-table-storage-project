e3da66d1d7e2a37196dfc4ddb16e206b66e0b1ba
Atul Kamble <atul_kamble@hotmail.com>
2026-03-22 11:34:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from azure.data.tables import TableServiceClient
+
+connection_string = "DefaultEndpointsProtocol=https;AccountName=mystoragetable98659080;AccountKey=bFRlgjos1qJx+GB86ub9LYrHZQVGvVuZvH65X0OE21UhluBe6NCFKimYk7PEE3HazS7nsMgzrQUF+AStM39MoQ==;EndpointSuffix=core.windows.net"
+
+service = TableServiceClient.from_connection_string(conn_str=connection_string)
+
+table_name = "mytable"
+table_client = service.get_table_client(table_name)
+
+entity = {
+    "PartitionKey": "Users",
+    "RowKey": "2",
+    "Name": "Cloud Architect",
+    "Age": 32
+}
+
+table_client.create_entity(entity=entity)
+
+print("Entity inserted successfully!")
